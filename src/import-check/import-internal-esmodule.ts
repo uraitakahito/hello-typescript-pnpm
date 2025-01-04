36af7dab29ec3d441e239b73a996168daac96450
Takahito Urai <uraitakahito@gmail.com>
2025-01-05 07:46:07 +0900

    Refactor ES module imports and exports; remove unused default export and add named exports examples

--- src/import-check/import-internal-esmodule.ts
+++ src/import-check/import-internal-esmodule.ts
@@ -1,7 +1,35 @@
+/* eslint-disable import/no-duplicates */
+
+// MEMO:
 // When importing ES modules without using a bundler or transpiler, file extensions are required:
 //   https://nodejs.org/api/esm.html#esm_mandatory_file_extensions
-import message from './exported-esmodule.js';
 
-console.log('import-check-1.js');
+//
+// Named import without `default export`:
+//
+// Good:
+import { named1 } from './named-exports-no-default.js';
+import * as namespaceObject1 from './named-exports-no-default.js';
+// Bad: SyntaxError
+// import defaultExport1 from './named-exports-no-default.esm.js';
 
-console.log(message);
+//
+// Named import with `default export`:
+//
+//   ALERT:
+//     DO NOT USE default export
+//     https://engineering.linecorp.com/ja/blog/you-dont-need-default-export
+//
+// Not bad:
+import { named3 } from './named-exports-with-default.js';
+import * as namespaceObject2 from './named-exports-with-default.js';
+import defaultExport2 from './named-exports-with-default.js';
+
+// Named import without `default export`:
+console.log(named1);
+console.log(namespaceObject1.named1, namespaceObject1.named2);
+
+// Named import with `default export`:
+console.log(named3);
+console.log(namespaceObject2.named3, namespaceObject2.default);
+console.log(defaultExport2);
